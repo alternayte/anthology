5590a04cd4aa0712fe9fb99d2e124c5b44ae0367
AlterNayte <47716887+AlterNayte@users.noreply.github.com>
2026-06-05 01:24:06 +0200

    feat: migration for streams table, checkpoints table, xid column, and events FK

--- src/Anthology/Kernel/EventStore/Migrations/20260604232343_EventStore_StreamsAndCheckpoints.Designer.cs
+++ src/Anthology/Kernel/EventStore/Migrations/20260604232343_EventStore_StreamsAndCheckpoints.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Anthology.Kernel.EventStore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Anthology.Kernel.EventStore.Migrations
 {
     [DbContext(typeof(EventStoreDbContext))]
-    partial class EventStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260604232343_EventStore_StreamsAndCheckpoints")]
+    partial class EventStore_StreamsAndCheckpoints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
